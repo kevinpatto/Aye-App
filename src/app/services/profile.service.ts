2fa6011da9396ad6419ee4b7bcc63799564891f4
Jonah Toch <jonahtoch@gmail.com>
2023-03-08 18:31:07 -0600

    try 1

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -21,7 +21,7 @@ export class ProfileService {
 
   updateProfilePicture(userId: string | undefined, authToken: string | undefined): void {
     // const url = `https://${environment.auth0Domain}/api/v2/users/` + userId;
-    const url = 'https://dev-mn6falogt3c14mat.us.auth0.com/';
+    const url = 'https://dev-mn6falogt3c14mat.us.auth0.com/api/v2/users/google-oauth2|101612299378795872117';
     // console.log(userId, authToken)
 
     // document.getElementById('callApi')!.addEventListener('click', async () => {
@@ -34,7 +34,8 @@ export class ProfileService {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': 'https://dev-mn6falogt3c14mat.us.auth0.com/api/v2/users/',
+        'Access-Control-Allow-Origin': '*',
+        'Authorization': 'Bearer eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIiwiaXNzIjoiaHR0cHM6Ly9kZXYtbW42ZmFsb2d0M2MxNG1hdC51cy5hdXRoMC5jb20vIn0..ymc45fgAcawkoUWs.7UaDSHjotLmzwabSH1Fpclvs6paR57AmszaxaAwr4acH3_yEyHN6G2DSMAAFO_or0TwIU9gukVpy5nk-wwTl7DjcCIO2PIsp0TmXjIAzw_S8orRxHrcoIeBlZ3PAoaf7EL1uqFTflgN_2IMLUVSNHErrPNq5z8HX66QRYHQXXY1vo_-uNeYgZAf5AZCLTQ8HRFfkZYA4-WlGerrsVTv76Cj8PZltco05SkuWZDm_wK1LYFGtf8tAclYUIMLbEjnd_GgOKVTBWzq9Sb-uxGqYjs-mPC6hOP8mUD9yzaXPkmtkwW84CSZjRY9ydTvhCYM2psqhUaE7kxIs1LJr6VRtizP4.5wNSwPQZMoxlel9nbacCcg'
       })
     };
     this.http.patch<any>(url, body, httpOptions).subscribe(
